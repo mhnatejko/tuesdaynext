f79a982ca5648b483072cb17caa7f30731a10c75
Maksymilian Hnatejko <maksymilian.hnatejko@netguru.com>
2021-11-30 13:07:26 +0100

    init next project

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.index_container}>
+      the app
+      <Link href='/page'><a>page</a></Link>
+    </div>
+  )
+}
+
+export default Home

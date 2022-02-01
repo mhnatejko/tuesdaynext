ebd4bd5eba6eef0a61a4cce92758625478c5b4b4
Maksymilian Hnatejko <maksymilian.hnatejko@gmail.com>
2022-02-01 11:25:56 +0100

    remove charset to avoid vercel error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
             defer
             src='//js-eu1.hs-scripts.com/25523533.js'
           />
-          <script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/shell.js"></script>
+          <script type="text/javascript" src="//js-eu1.hsforms.net/forms/shell.js"></script>
           <script>
             hbspt.forms.create({
             region: "eu1",

9d01a9abb0482bdd0b225362bdb6569d7a813bee
Maksymilian Hnatejko <maksymilian.hnatejko@gmail.com>
2022-02-01 10:42:16 +0100

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* <!-- Start of HubSpot Embed Code --> */}
+          <script
+            type='text/javascript'
+            id='hs-script-loader'
+            async
+            defer
+            src='//js-eu1.hs-scripts.com/25523533.js'
+          />
+          {/* <!-- End of HubSpot Embed Code --> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}

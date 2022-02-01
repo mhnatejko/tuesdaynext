f321b54967168defad08e49140c94b6207b802fc
Maksymilian Hnatejko <maksymilian.hnatejko@netguru.com>
2022-02-01 12:18:14 +0100

    feat: add hubspot scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
+export const fn = (x: number, _y: any) => x * 1;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Script
+        type='text/javascript'
+        src='//js-eu1.hsforms.net/forms/shell.js'
+      />
+      <Script
+        id='hsscript'
+        dangerouslySetInnerHTML={{
+          __html: `
+            hbspt.forms.create({
+              region: 'eu1',
+              portalId: '25523533',
+              formId: '77af856f-eaae-4f68-8484-c6d7661d68f7',
+            })
+            `,
+        }}
+      />
+    </>
+  );
 }
 
 export default MyApp;

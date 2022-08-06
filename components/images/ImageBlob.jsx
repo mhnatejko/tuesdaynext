e81db28ee75c5119f62e7fef8af1bd4f312b54f7
unknown <maksymilian.hnatejko@gmail.com>
2022-08-06 21:13:33 +0200

    refactor: prettier run

--- components/images/ImageBlob.jsx
+++ components/images/ImageBlob.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 
 const ImageBlob = ({ color1, color2 }) => {
-    return (     
-    <svg
-        viewBox="0 0 1000 1000"
-    >
-        <defs>
-            <linearGradient id="linearGradientId" gradientTransform="rotate(0 0.5 0.5)">
-                <stop offset="0%"  stop-color={color1} />
-                <stop offset="100%"  stop-color={color2} />
-            </linearGradient>
-            <clipPath id="shape">
-                <path fill="currentColor" d="M854,592.5Q755,685,665.5,709Q576,733,505,718.5Q434,704,400,651Q366,598,308.5,549Q251,500,232.5,396Q214,292,305,236.5Q396,181,518.5,123Q641,65,772,136Q903,207,928,353.5Q953,500,854,592.5Z"></path>
-            </clipPath>
-        </defs>
-        <g  clip-path="url(#shape)">
-            <path fill="url(#linearGradientId)" d="M854,592.5Q755,685,665.5,709Q576,733,505,718.5Q434,704,400,651Q366,598,308.5,549Q251,500,232.5,396Q214,292,305,236.5Q396,181,518.5,123Q641,65,772,136Q903,207,928,353.5Q953,500,854,592.5Z" />
-        </g>
+  return (
+    <svg viewBox='0 0 1000 1000'>
+      <defs>
+        <linearGradient
+          id='linearGradientId'
+          gradientTransform='rotate(0 0.5 0.5)'
+        >
+          <stop offset='0%' stop-color={color1} />
+          <stop offset='100%' stop-color={color2} />
+        </linearGradient>
+        <clipPath id='shape'>
+          <path
+            fill='currentColor'
+            d='M854,592.5Q755,685,665.5,709Q576,733,505,718.5Q434,704,400,651Q366,598,308.5,549Q251,500,232.5,396Q214,292,305,236.5Q396,181,518.5,123Q641,65,772,136Q903,207,928,353.5Q953,500,854,592.5Z'
+          ></path>
+        </clipPath>
+      </defs>
+      <g clip-path='url(#shape)'>
+        <path
+          fill='url(#linearGradientId)'
+          d='M854,592.5Q755,685,665.5,709Q576,733,505,718.5Q434,704,400,651Q366,598,308.5,549Q251,500,232.5,396Q214,292,305,236.5Q396,181,518.5,123Q641,65,772,136Q903,207,928,353.5Q953,500,854,592.5Z'
+        />
+      </g>
     </svg>
-    );
+  );
 };
 
 export default ImageBlob;
--- views/Home/Home.jsx
+++ views/Home/Home.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import * as styles from './home.module.css';
-import ImageCityLife from '../../components/images/ImageCityLife'
-import ImageLogistics from '../../components/images/ImageLogistics'
-import ImageBlob from '../../components/images/ImageBlob'
-import ImageUnderConstruction from '../../components/images/ImageUnderConstruction'
+import ImageCityLife from '../../components/images/ImageCityLife';
+import ImageLogistics from '../../components/images/ImageLogistics';
+import ImageBlob from '../../components/images/ImageBlob';
+import ImageUnderConstruction from '../../components/images/ImageUnderConstruction';
 
 const Home = () => {
-    const primaryColor = '#91d9af';
-    const primaryColorDarker = '#94cdac';
-    const secondaryColor = '#dbb27d';
-    const grayColor = '#dcf4e6';
-    const grayColorDarker = '#a0c9b1';
-    
+  const primaryColor = '#91d9af';
+  const primaryColorDarker = '#94cdac';
+  const secondaryColor = '#dbb27d';
+  const grayColor = '#dcf4e6';
+  const grayColorDarker = '#a0c9b1';
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.blob1}>
-                <ImageBlob color1={grayColor} color2={grayColorDarker} />
-            </div>
-            <div className={styles.blob2}>
-                <ImageBlob color1={primaryColor} color2={primaryColorDarker} />
-            </div>
-            <div className={styles.text}>
-                <h1>Page is under construction</h1>
-                <p>Please come back later</p>
-            </div>
-            <div className={styles.picture}>
-                <ImageCityLife color={primaryColor}/>
-                {/* <ImageUnderConstruction color={primaryColor}/> */}
-                {/* <ImageLogistics color={primaryColor}/> */}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.blob1}>
+        <ImageBlob color1={grayColor} color2={grayColorDarker} />
+      </div>
+      <div className={styles.blob2}>
+        <ImageBlob color1={primaryColor} color2={primaryColorDarker} />
+      </div>
+      <div className={styles.text}>
+        <h1>Page is under construction</h1>
+        <p>Please come back later</p>
+      </div>
+      <div className={styles.picture}>
+        <ImageCityLife color={primaryColor} />
+        {/* <ImageUnderConstruction color={primaryColor}/> */}
+        {/* <ImageLogistics color={primaryColor}/> */}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
